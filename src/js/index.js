f4b8f1131d11a1f2bfd46e532ad1e03cb331b477
misswang <571049688@qq.com>
2018-04-13 15:55:53 +0800

    代码重构功能

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,32 @@
 $(function(){
+  // 处理列表内容
+  // 加载列表数据
+  function loadListData(){
+    return axios.get('home/goodslist');
+  }
+  // 渲染列表
+  function renderList(param){
+    return new Promise(function(resolve,reject){
+      let html = template('listTpl', param.data);
+      $('#listInfo').html(html);
+      resolve();
+    })
+  }
 
-
+  // 处理菜单
+  // 加载菜单数据
+  function loadMenuData(){
+    return axios.get('home/catitems');
+  }
+  // 渲染菜单内容
+  function renderMenu(param){
+    return new Promise(function(resolve,reject){
+      let html = template('menuTpl',{list: param.data});
+      $('#menuInfo').html(html);
+      resolve();
+    });
+  }
+  // --------------------------------------------
   // 处理轮播
   // 获取轮播图数据
   function loadSwiperData(){
@@ -14,6 +40,7 @@ $(function(){
       resolve();
     });
   }
+  // 启用轮播效果
   function handleSwiper(){
     return new Promise(function(resolve,reject){
       new Swiper ('.swiper-container', {
@@ -38,6 +65,18 @@ $(function(){
       .catch(function(){
         $.toast('服务器错误');
       })
+    // 处理菜单
+    loadMenuData()
+      .then(renderMenu)
+      .catch(function(){
+        $.toast('服务器错误');
+      });
+    // 处理列表
+    loadListData()
+      .then(renderList)
+      .catch(function(){
+        $.toast('服务器错误');
+      });
   });
   $.init();
 });
